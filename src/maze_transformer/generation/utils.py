cc0255ca3954ecf6a34de3604164bef14a10819c
Rusheb <rusheb.shah@gmail.com>
2023-02-01 15:40:38 +0000

    Add docstring

--- src/maze_transformer/generation/utils.py
+++ src/maze_transformer/generation/utils.py
@@ -1,9 +1,34 @@
 import math
 
 import numpy as np
+from muutils.tensor_utils import NDArray
 
 
-def bool_array_from_string(string: str, shape: list[int], true_symbol="T"):
+def bool_array_from_string(string: str, shape: list[int], true_symbol:str="T") -> NDArray:
+    """Transform a string into an ndarray of bools.
+
+    Parameters
+    ----------
+    string: str
+        The string representation of the array
+    shape: list[int]
+        The shape of the resulting array
+    true_symbol:
+        The character to parse as True. Whitespace will be removed. All other characters will be parsed as False.
+
+    Returns
+    -------
+    NDArray
+        A ndarray with dtype bool.
+
+    Examples
+    --------
+    >>> bool_array_from_string(
+    ...     "TT TF", shape=[2,2]
+    ... )
+    array([[ True,  True],
+        [ True, False]])
+    """
     stripped = "".join(string.split())
 
     expected_symbol_count = math.prod(shape)
