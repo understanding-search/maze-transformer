8117543a03faed2390b4e2871a8fc0c44f31fd0d
Rusheb <rusheb.shah@gmail.com>
2023-02-13 18:10:36 +0000

    WIP refactor

--- src/maze_transformer/training/config.py
+++ src/maze_transformer/training/config.py
@@ -1,3 +1,4 @@
+from __future__ import annotations
 from dataclasses import dataclass, field
 from typing import Annotated, Any, Callable
 
@@ -8,6 +9,9 @@ from muutils.json_serialize import (
 )
 from muutils.tensor_utils import DTYPE_MAP, TORCH_OPTIMIZERS_MAP
 from transformers import OpenAIGPTConfig
+
+from maze_transformer.training.dataset import GPTDatasetConfig
+from transformer_lens import HookedTransformerConfig
 
 DEVICE_OVERRIDE: torch.device | None = (
     torch.device("cuda:0") if torch.cuda.is_available() else None
@@ -20,42 +24,19 @@ TokenizerFunction = Callable[[list[str]], list[int]]
 
 
 @dataclass(frozen=True, kw_only=True)
-class BaseGPTConfig:
-    """gpt model config without vocab size, context size, or padding token
-
-    TODO: honestly this complexity is pointless, refactor to a dict?
+class BaseGPTConfig(HookedTransformerConfig):
+    """
+    Add a name property and serialization to HookedTransformerConfig
     """
 
-    gpt_cfg_name: str
-    n_embed: int = 128
-    n_layer: int = 8
-    n_head: int = 4
-    _kwargs: dict = field(default_factory=dict)
-
-    def as_dict(self) -> dict:
-        return dict(
-            **{
-                k: getattr(self, k) for k in self.__dataclass_fields__ if k != "_kwargs"
-            },
-            **self._kwargs,
-        )
+    name: str
 
     def serialize(self) -> str:
-        return self.as_dict()
+        return self.to_dict()
 
-    def load(self, d: dict) -> "BaseGPTConfig":
-        """load from dict, putting unknown fields into _kwargs"""
-        ctor_kwargs: dict = {
-            k: v for k, v in d.items() if k in self.__dataclass_fields__
-        }
-        extra_kwargs: dict = {
-            k: v for k, v in d.items() if k not in self.__dataclass_fields__
-        }
-
-        return BaseGPTConfig(
-            **ctor_kwargs,
-            _kwargs=extra_kwargs,
-        )
+    @classmethod
+    def load(cls, d: dict) -> BaseGPTConfig:
+        return cls.from_dict(d)
 
 
 # ==================================================
@@ -67,14 +48,6 @@ class TrainConfig:
 
     name: str
 
-    base_gpt_cfg: BaseGPTConfig
-    # wandb_proj: str|None = None
-    device: Annotated[torch.device, "device to use for training"] = torch.device(
-        # "cuda" if torch.cuda.is_available() else "cpu"
-        "cuda:0"
-    )
-
-    max_samples: int | None = None
     epochs: int = 1
     optimizer: torch.optim.Optimizer = torch.optim.RMSprop
     optimizer_kwargs: dict[str, Any] = field(default_factory=lambda: dict(lr=0.000001))
@@ -94,108 +67,49 @@ class TrainConfig:
     print_loss_interval: int = 1000
     checkpoint_interval: int = 50000
 
-    seq_len_max: int | None = None
-
-    # n_ctx: int = property(lambda self: self.model_config.n_ctx)
-    _gpt_config_ctor_kwargs: dict | None = None
-
-    def get_gpt_config(
-        self,
-        **kwargs,
-    ) -> OpenAIGPTConfig:
-        """passes base_gpt_cfg, device, and _gpt_config_ctor_kwargs to OpenAIGPTConfig"""
-
-        if self._gpt_config_ctor_kwargs is not None:
-            if len(kwargs) > 0:
-                raise ValueError("gpt_config_ctor_kwargs already set!")
-            else:
-                pass
-                # the _gpt_config_ctor_kwargs is already set, so we just return it
-        else:
-            # generate the _gpt_config_ctor_kwargs
-            self._gpt_config_ctor_kwargs = {
-                **self.base_gpt_cfg.as_dict(),
-                **dict(device=self.device),
-                **kwargs,
-            }
-
-        return OpenAIGPTConfig(**self._gpt_config_ctor_kwargs)
-
 
 TrainConfig.serialize = dataclass_serializer_factory(
     TrainConfig,
     special_serializers=dict(
-        # gpt_config = lambda self: json_serialize(self.get_gpt_config().to_dict()),
         _optimizer_name=lambda self: self.optimizer.__name__,
-        base_gpt_cfg=lambda self: self.base_gpt_cfg.as_dict(),
-        device=lambda self: str(self.device),
     ),
     fields_exclude=["optimizer"],
 )
-
-
-def load_device(d: dict | str) -> torch.device:
-    if DEVICE_OVERRIDE is not None:
-        return DEVICE_OVERRIDE
-    elif isinstance(d, str):
-        return torch.device(d)
-    elif isinstance(d, dict):
-        return torch.device(d["str"])
-    else:
-        raise TypeError(
-            f"invalid type for loading device from serialization: {type(d) = } {d = }"
-        )
-
-
-def process_config_kwargs(kwargs: dict | None) -> dict | None:
-    """process config kwargs, converting device and dtype"""
-
-    if kwargs is None:
-        return None
-
-    if "device" in kwargs:
-        kwargs["device"] = load_device(kwargs["device"])
-
-    if "dtype" in kwargs:
-        kwargs["dtype"] = DTYPE_MAP[kwargs["dtype"]]
-
-    return kwargs
-
 
 TrainConfig.load = dataclass_loader_factory(
     TrainConfig,
     special_loaders=dict(
         optimizer=lambda d: TORCH_OPTIMIZERS_MAP[d["_optimizer_name"]],
-        base_gpt_cfg=lambda d: BaseGPTConfig(**d["base_gpt_cfg"]),
-        device=lambda d: load_device(d["device"]),
-        _gpt_config_ctor_kwargs=lambda d: process_config_kwargs(
-            d.get("_gpt_config_ctor_kwargs", None)
-        ),
     ),
 )
-
 
 # actual configuration setups
 # ==================================================
 
+# TODO: modify existing configurations (base and training) below to work with the updated classes.
+# write tests: 
+    # - loading and serialization of TrainConfig, BaseGPTConfig, TopLevelConfig
+    # - make sure we can instantiate HookedTransformer using BaseGPTConfig
+    # - get `train`, `eval_model`, `plot_attention`` to run`
+    # - run train function for a single gradient update (sanity check) - make sure it doesn't crash
+
+
 _GPT_CONFIGS_LIST: list[BaseGPTConfig] = [
     BaseGPTConfig(
-        gpt_cfg_name="tiny-v1",
-        n_embed=32,
-        n_layer=4,
-        n_head=2,
+        name="tiny-v1",
+        d_model=32,
+        n_layers=4,
+        n_heads=2,
     ),
     BaseGPTConfig(
-        gpt_cfg_name="medium-v1",
-        n_embed=128,
-        n_layer=8,
-        n_head=4,
+        name="medium-v1",
+        d_model=128,
+        n_layers=8,
+        n_heads=4,
     ),
 ]
 
-GPT_CONFIGS: dict[str, BaseGPTConfig] = {
-    cfg.gpt_cfg_name: cfg for cfg in _GPT_CONFIGS_LIST
-}
+GPT_CONFIGS: dict[str, BaseGPTConfig] = {cfg.name: cfg for cfg in _GPT_CONFIGS_LIST}
 
 _TRAINING_CONFIG_LIST: list[TrainConfig] = [
     TrainConfig(
@@ -220,3 +134,14 @@ _TRAINING_CONFIG_LIST: list[TrainConfig] = [
 TRAINING_CONFIGS: dict[str, TrainConfig] = {
     cfg.name: cfg for cfg in _TRAINING_CONFIG_LIST
 }
+
+
+@dataclass
+class TopLevelConfig:
+    """
+    Handles any logic that moves data between the configs below it.
+    """
+
+    train_config: TrainConfig | None
+    dataset_config: GPTDatasetConfig | None
+    model_config: BaseGPTConfig | None
