bda7b76aa8bb21e1e90db4bdb828e2bbb7683bfa
Rusheb <rusheb.shah@gmail.com>
2023-02-14 13:59:48 +0000

    Test TrainConfig

--- src/maze_transformer/training/config.py
+++ src/maze_transformer/training/config.py
@@ -1,17 +1,17 @@
 from __future__ import annotations
+
 from dataclasses import dataclass, field
-from typing import Annotated, Any, Callable
+from typing import Any, Callable, ClassVar, Type
 
 import torch
-from muutils.json_serialize import (
+from muutils.json_serialize import (  # type: ignore[import]
     dataclass_loader_factory,
     dataclass_serializer_factory,
 )
-from muutils.tensor_utils import DTYPE_MAP, TORCH_OPTIMIZERS_MAP
-from transformers import OpenAIGPTConfig
+from muutils.tensor_utils import DTYPE_MAP, TORCH_OPTIMIZERS_MAP  # type: ignore[import]
+from transformer_lens import HookedTransformerConfig  # type: ignore[import]
 
 from maze_transformer.training.dataset import GPTDatasetConfig
-from transformer_lens import HookedTransformerConfig
 
 DEVICE_OVERRIDE: torch.device | None = (
     torch.device("cuda:0") if torch.cuda.is_available() else None
@@ -22,14 +22,16 @@ TokenizerFunction = Callable[[list[str]], list[int]]
 
 # ==================================================
 
-
-@dataclass(frozen=True, kw_only=True)
+@dataclass(kw_only=True)
 class BaseGPTConfig(HookedTransformerConfig):
     """
     Add a name property and serialization to HookedTransformerConfig
     """
 
     name: str
+    # TODO:HookedTransformer requires that either d_mlp == None or act_fn != None
+    # This might be the wrong choice of default.
+    act_fn: str | None = "relu"
 
     def serialize(self) -> str:
         return self.to_dict()
@@ -41,7 +43,6 @@ class BaseGPTConfig(HookedTransformerConfig):
 
 # ==================================================
 
-
 @dataclass(kw_only=True)
 class TrainConfig:
     """full training configuration"""
@@ -49,7 +50,7 @@ class TrainConfig:
     name: str
 
     epochs: int = 1
-    optimizer: torch.optim.Optimizer = torch.optim.RMSprop
+    optimizer: Type[torch.optim.Optimizer] = torch.optim.RMSprop
     optimizer_kwargs: dict[str, Any] = field(default_factory=lambda: dict(lr=0.000001))
     batch_size: int = 128
 
@@ -67,45 +68,43 @@ class TrainConfig:
     print_loss_interval: int = 1000
     checkpoint_interval: int = 50000
 
-
-TrainConfig.serialize = dataclass_serializer_factory(
+TrainConfig.serialize = dataclass_serializer_factory( # type: ignore[attr-defined]
     TrainConfig,
     special_serializers=dict(
-        _optimizer_name=lambda self: self.optimizer.__name__,
+        optimizer=lambda self: self.optimizer.__name__,
     ),
-    fields_exclude=["optimizer"],
 )
 
-TrainConfig.load = dataclass_loader_factory(
+TrainConfig.load = dataclass_loader_factory( # type: ignore[attr-defined]
     TrainConfig,
     special_loaders=dict(
-        optimizer=lambda d: TORCH_OPTIMIZERS_MAP[d["_optimizer_name"]],
+        optimizer=lambda d: TORCH_OPTIMIZERS_MAP[d["optimizer"]],
     ),
 )
+
 
 # actual configuration setups
 # ==================================================
 
 # TODO: modify existing configurations (base and training) below to work with the updated classes.
-# write tests: 
-    # - loading and serialization of TrainConfig, BaseGPTConfig, TopLevelConfig
-    # - make sure we can instantiate HookedTransformer using BaseGPTConfig
-    # - get `train`, `eval_model`, `plot_attention`` to run`
-    # - run train function for a single gradient update (sanity check) - make sure it doesn't crash
+# write tests:
+# - loading and serialization of
+#    - TrainConfig
+#    - BaseGPTConfig
+#    - TopLevelConfig
+# - make sure we can instantiate HookedTransformer using BaseGPTConfig
+# - get `train`, `eval_model`, `plot_attention`` to run`
+# - run train function for a single gradient update (sanity check) - make sure it doesn't crash
 
 
+# TODO: michael check these values
 _GPT_CONFIGS_LIST: list[BaseGPTConfig] = [
     BaseGPTConfig(
         name="tiny-v1",
         d_model=32,
+        d_head=16,
+        n_ctx=90,
         n_layers=4,
-        n_heads=2,
-    ),
-    BaseGPTConfig(
-        name="medium-v1",
-        d_model=128,
-        n_layers=8,
-        n_heads=4,
     ),
 ]
 
@@ -114,7 +113,6 @@ GPT_CONFIGS: dict[str, BaseGPTConfig] = {cfg.name: cfg for cfg in _GPT_CONFIGS_L
 _TRAINING_CONFIG_LIST: list[TrainConfig] = [
     TrainConfig(
         name="tiny-v1",
-        base_gpt_cfg=GPT_CONFIGS["tiny-v1"],
         optimizer=torch.optim.RMSprop,
         optimizer_kwargs=dict(lr=0.000001),
         batch_size=32,
@@ -126,7 +124,6 @@ _TRAINING_CONFIG_LIST: list[TrainConfig] = [
         ),
         print_loss_interval=1000,
         checkpoint_interval=5000,
-        seq_len_max=90,
     )
 ]
 
@@ -141,7 +138,6 @@ class TopLevelConfig:
     """
     Handles any logic that moves data between the configs below it.
     """
-
     train_config: TrainConfig | None
     dataset_config: GPTDatasetConfig | None
     model_config: BaseGPTConfig | None
