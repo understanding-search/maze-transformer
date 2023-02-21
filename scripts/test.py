e7cf36ab58932fb107b8de1c1579e934003c7817
Rusheb <rusheb.shah@gmail.com>
2023-02-21 11:48:55 +0000

    WIP

---
+++ scripts/test.py
@@ -0,0 +1,9 @@
+from maze_transformer.training.tokenizer import MazeTokenizer
+print("hello world")
+
+c = MazeTokenizer()
+
+"""
+TODO:
+- import training.config.BaseGPTConfig (test dependency on muutils)
+"""
