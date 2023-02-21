56382a2bada5cd1c8d5f1ca1a3e778e9a0996d1d
Rusheb <rusheb.shah@gmail.com>
2023-02-21 12:00:12 +0000

    Run unit tests

--- scripts/test.py
+++
@@ -1,9 +0,0 @@
-from maze_transformer.training.tokenizer import MazeTokenizer
-print("hello world")
-
-c = MazeTokenizer()
-
-"""
-TODO:
-- import training.config.BaseGPTConfig (test dependency on muutils)
-"""
