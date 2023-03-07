44c948b753b72a7057fec0f8a59eec611ab6056b
Alex Spies <alex@afspies.com>
2023-03-07 17:07:05 +0000

    sync commit

--- maze_transformer/training/tokenizer.py
+++ maze_transformer/training/tokenizer.py
@@ -60,3 +60,65 @@ class MazeTokenizer:
             )
 
         return tokens
+
+from transformers import PreTrainedTokenizer
+from muutils.tensor_utils import ATensor, NDArray
+from maze_transformer.training.config import ConfigHolder
+from maze_transformer.generation.latticemaze import SPECIAL_TOKENS
+# from maze_transformer.training.mazedataset import MazeDatasetConfig
+
+class HuggingMazeTokenizer(PreTrainedTokenizer):
+    vocab: dict[str, int]# map of token_ids to strings
+
+    bos_token: str = SPECIAL_TOKENS["padding"]
+    eos_token: str = SPECIAL_TOKENS["padding"] 
+    unk_token: str = '<WOOPS>'
+    #! pad_token is set to tokenizer.eos_token by TransformerLens
+    # TODO check if this is a problem for us
+    pad_token: str = eos_token
+    additional_special_tokens: list[str] = [x for x in SPECIAL_TOKENS.values() if x not in [SPECIAL_TOKENS['padding']]]
+    
+    # IDs specified during construction
+    # bos_token_id: int
+    # eos_token_id: int
+    # pad_token_id: int
+
+    # Overwrite class attributes
+    padding_side = "left"
+    truncation_side = "left" #! strange choice, but it's what we did in pad_sequence
+    
+    name_or_path = "maze_tokenizer"
+    
+    def __init__(self, cfg: ConfigHolder, **kwargs):
+        super().__init__(max_len=cfg.dataset_cfg.seq_len_max, **kwargs)
+        self._add_tokens()
+        vocab = {k: v for v, k in enumerate(cfg.dataset_cfg.token_arr)}
+        self.added_tokens_encoder = vocab
+        self.added_tokens_decoder = {v: k for k, v in vocab.items()}
+        self.vocab = vocab #! Feels like this should be automatic through the super().__init__ call
+        
+        self.bos_token_id = self.vocab[self.bos_token]
+        self.eos_token_id = self.vocab[self.eos_token]
+        self.pad_token_id = self.vocab[self.pad_token]
+
+    # def __call__(self, str_tokens: list[str], **kwargs) -> ATensor:
+    #     tokens = [self.vocab[token] for token in maze.as_tokens(cfg.node_token_map)]
+    #     maze_tokens = self.encode(tokens, **kwargs)
+    #     return maze_tokens
+
+    # def batch_decode(self, sequences: list[int] | list[list[int]] | ATensor, skip_special_tokens: bool = False, **kwargs) -> list[str]:
+    #     return super().batch_decode(sequences, skip_special_tokens, clean_up_tokenization_spaces=False, **kwargs)
+
+    # def decode(self, sequence: list[int] | ATensor, skip_special_tokens: bool = False, **kwargs) -> str:
+    #     return super().decode(sequence, skip_special_tokens, clean_up_tokenization_spaces=False, **kwargs)
+    
+    def to_image(self, sequence: list[int] | ATensor | list[str], **kwargs) -> NDArray:
+
+        if isinstance(sequence, list) and isinstance(sequence[0], str):
+            str_sequence = sequence # already decoded
+        else:
+            str_sequence = self.decode(sequence)
+
+        lattice_maze = LatticeMaze.from_tokens(str_sequence) 
+        return lattice_maze.as_img(**kwargs)
+   
--- tests/unit/maze_transformer/generation/test_tokenizers.py
+++ tests/unit/maze_transformer/generation/test_tokenizers.py
@@ -4,11 +4,35 @@ as the original tokenizer (i.e. just using the token map in cfg)
 
 We may want a separate set of tests for different tokenization schemes
 """
-# TODO - Pending tokenizer
-
+from maze_transformer.training.tokenizer import MazeTokenizer, HuggingMazeTokenizer
+from maze_transformer.training.mazedataset import MazeDatasetConfig
+from maze_transformer.training.config import ConfigHolder
+from scripts.create_dataset import generate_MazeTokenizer
 
 def test_tokenization_encoding():
     # Check that wrapped tokenizer __call__ returns the same as original tokenizer
+    maze = generate_MazeTokenizer(None, 3, (0, 0), (2, 1))
+
+    # Need to generate a config to extract the token map >.<
+    cfg = MazeDatasetConfig(name='testing_maze', grid_n = 3, n_mazes=1)
+    node_token_map = cfg.node_token_map
+    
+    # Adjacency List Tokenization  
+    maze_str_tokens = maze.as_tokens(node_token_map) 
+
+    # Manual Tokenization
+    vocab_map = {k: v for v, k in enumerate(cfg.token_arr)}
+    maze_tokens = [vocab_map[token] for token in maze_str_tokens]
+    
+    # WrappedTokenizer
+    # Initialized with a configholder - tokenizer will eventually be a string
+    cfg_holder = ConfigHolder(train_cfg=None, dataset_cfg=cfg, model_cfg=None, tokenizer=None)
+    tokenizer = HuggingMazeTokenizer(cfg_holder)
+    print(tokenizer(maze_str_tokens))
+
+
+def test_ascii_encoding():
+    # Check that the ascii encoding works for multiple different inputs
     pass
 
 def test_tokenization_decoding():
@@ -21,4 +45,9 @@ def test_padding():
 
 def test_inside_hooked_transformer():
     # Check that the wrapped tokenizer facilitates all HookedTransformer tokenizer-dependent functionality
+    maze_id_tokens = tokenizer.to_tokens(maze_str_tokens)
+    maze_id_tokens
     pass
+
+
+test_tokenization_encoding()
