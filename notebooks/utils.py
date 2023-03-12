e1e47c8f88ad7db9e5ee0b11d6b6d4e71df6cdce
Lucia Quirke <luciaq@canva.com>
2023-03-12 11:13:57 +1100

    fixup

--- notebooks/utils.py
+++ notebooks/utils.py
@@ -1,46 +1,36 @@
-import os
-from pathlib import Path
-
 import matplotlib.pyplot as plt
 import plotly.io as pio
 import torch
 from IPython import get_ipython
 
 
-def initialize_notebook(run_path=Path("../data/maze/g4-n10"), seed=42, dark_mode=True):
+def configure_notebook(seed=42, dark_mode=True):
     """Shared Jupyter notebook setup steps:
     - Set random seed
-    - Validate specified model path
     - Set device based on availability
     - Set module reloading before code execution
     - Set plot rendering and formatting
-    - Disable PyTorch gradients
     """
 
     # Set seed for reproducibility
     _ = torch.manual_seed(seed)
 
-    # Get latest model
-    assert run_path.exists(), f"Run path {run_path.as_posix()} does not exist"
-    model_path = list(sorted(run_path.glob("**/model.final.pt"), key=os.path.getmtime))[
-        -1
-    ].resolve()
-
     # Set device
-    if torch.backends.mps.is_available():
-        device = torch.device("mps")
-        print("Device set to MPS")
-    elif torch.cuda.is_available():
+    if torch.cuda.is_available():
         device = torch.device("cuda")
         print("Device set to CUDA")
+    elif torch.backends.mps.is_available():
+        device = torch.device("mps")
+        print("Device set to MPS")
     else:
         device = torch.device("cpu")
         print("Device set to CPU")
 
     # Reload modules before executing user code
     ipython = get_ipython()
-    ipython.magic("load_ext autoreload")
-    ipython.magic("autoreload 2")
+    if "IPython.extensions.autoreload" not in ipython.extension_manager.loaded:
+        ipython.magic("load_ext autoreload")
+        ipython.magic("autoreload 2")
 
     # Specify plotly renderer for vscode
     pio.renderers.default = "notebook_connected"
@@ -49,7 +39,4 @@ def initialize_notebook(run_path=Path("../data/maze/g4-n10"), seed=42, dark_mode
         pio.templates.default = "plotly_dark"
         plt.style.use("dark_background")
 
-    # We won't be training any models
-    torch.set_grad_enabled(False)
-
-    return device, model_path
+    return device
