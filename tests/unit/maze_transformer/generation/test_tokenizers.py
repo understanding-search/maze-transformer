5914fffd9358ebc88ee9d31a7bcc08f768d40963
Alex Spies <alex@afspies.com>
2023-03-13 11:55:22 +0000

    merge rest

--- tests/unit/maze_transformer/generation/test_tokenizers.py
+++ tests/unit/maze_transformer/generation/test_tokenizers.py
@@ -12,6 +12,7 @@ from maze_transformer.training.mazedataset import MazeDatasetConfig
 from maze_transformer.training.tokenizer import HuggingMazeTokenizer
 from scripts.create_dataset import generate_MazeTokenizer
 
+from pytest import mark, param
 
 def test_tokenization_encoding():
     # Check that wrapped tokenizer __call__ returns the same as original tokenizer
@@ -246,3 +247,30 @@ def test_inside_hooked_transformer():
     assert torch.all(
         token_ids_2.cpu() == torch.tensor(batched_tokens_manual)
     ), "Batched tokenization encoding inside HookedTransformer failed"
+
+
+# Padding Tests
+PADDING_ID = 8
+test_data = [
+    param([1, 2, 3], [PADDING_ID, PADDING_ID, 1, 2, 3], id="short"),
+    param([1, 2, 3, 4, 5], [1, 2, 3, 4, 5], id="max_length"),
+    param([1, 2, 3, 4, 5, 6], [2, 3, 4, 5, 6], id="too_long"),
+]
+
+
+@mark.parametrize("inp,expected", test_data)
+def test_pad_sequence_param(inp, expected):
+    # Initialized with a configholder - tokenizer will eventually be a string
+    cfg = MazeDatasetConfig(name="testing_maze", grid_n=3, n_mazes=1)
+    cfg_holder = ConfigHolder(
+        train_cfg=None, dataset_cfg=cfg, model_cfg=None, tokenizer=None
+    )
+    tokenizer = HuggingMazeTokenizer(cfg_holder)
+
+    # Need to go to string representation to pad
+    inp = tokenizer.decode(inp)
+    result = tokenizer(
+        inp, padding="max_length", truncation=True, max_length=5, return_tensors="pt"
+    )["input_ids"][0]
+
+    assert torch.equal(result, torch.tensor(expected))
