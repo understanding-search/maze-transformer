cef043ea953f228fc9812cfc3cffb14df70b6b58
mivanit <mivanits@umich.edu>
2023-03-14 11:06:53 -0600

    general tidying up

--- tests/integration/maze_transformer/generation/test_plot_loss.py
+++ tests/integration/maze_transformer/generation/test_plot_loss.py
@@ -1,5 +1,4 @@
 import os
-import tempfile
 from pathlib import Path
 
 import pytest
@@ -9,20 +8,14 @@ from scripts.create_dataset import create_dataset
 from scripts.train_model import train_model
 
 
-@pytest.fixture()
-def temp_dir() -> Path:
-    data_dir = tempfile.TemporaryDirectory()
-    yield Path(data_dir.name)
-    data_dir.cleanup()
-
-
+@pytest.mark.usefixtures("temp_dir")
 def test_plot_loss(temp_dir):
-    n_mazes = 25
-    grid_n = 3
-    name = "test"
+    n_mazes: int = 25
+    grid_n: int = 3
+    name: str = "test"
 
     create_dataset(path_base=str(temp_dir), n_mazes=n_mazes, grid_n=grid_n, name=name)
-    dataset_directory_name = f"g{grid_n}-n{n_mazes}-{name}"
+    dataset_directory_name: str = f"g{grid_n}-n{n_mazes}-{name}"
 
     train_model(
         basepath=str(temp_dir / dataset_directory_name),
@@ -30,10 +23,12 @@ def test_plot_loss(temp_dir):
         model_cfg="nano-v1",
     )
 
-    # hard mode: get log file name
-    list_subfolders_with_paths = [
+    # Log file name includes a timestamp so need to check the filesystem to determine the name
+    list_subfolders_with_paths: list[Path] = [
         f.path for f in os.scandir(temp_dir / dataset_directory_name) if f.is_dir()
     ]
-    log_file_path = temp_dir / list_subfolders_with_paths[0] / "log.jsonl"
+    log_file_path: Path = temp_dir / list_subfolders_with_paths[0] / "log.jsonl"
 
     plot_loss(str(log_file_path))
+
+    plot_loss(str(log_file_path), raw_loss="r.")
