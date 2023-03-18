fix off by one error in IndexedArray

maze_transformer/training/dataset.py
@@ -4,7 +4,6 @@ from functools import cached_property
 import numpy as np
 import torch
 from muutils.tensor_utils import ATensor
-from torch import Tensor
 from torch.utils.data import Dataset
 
 from maze_transformer.utils.utils import get_device
@@ -67,24 +66,30 @@ class GPTDatasetConfig:
 
 @dataclass(kw_only=True)
 class IndexedArray:
-    """join a list of arrays into a single big one with indices
+    """Contains a tensor made by concatenating a list of tensors, and a second tensor indicating the starting indices
+    of the original tensors in the first one. Mainly for getting __getitem__ to work nicely with datasets
 
-    mainly for allowing __getitem__ to work nice for datasets"""
+    arr: tensor containing all the elements of the original arrays: [1, 2], [3, 4] -> [1, 2, 3, 4]
+    idxs: tensor indicating the starting index in arr of each original array: [1, 2], [3, 4] -> [0, 2]
+    """
 
     arr: ATensor
-    indices: ATensor
+    idxs: ATensor
 
     def get_len(self, i: int) -> int:
-        return self.indices[i + 1] - self.indices[i]
+        if i + 1 < len(self.idxs):
+            return self.idxs[i + 1] - self.idxs[i]
+
+        return self.arr.size(0) - self.idxs[i]
 
     def get_all_lengths(self) -> ATensor:
         return torch.cat(
             [
-                self.indices[1:] - self.indices[:-1],
+                self.idxs[1:] - self.idxs[:-1],
                 torch.tensor(
-                    [self.arr.shape[0] - self.indices[-1]],
-                    dtype=self.indices.dtype,
-                    device=self.indices.device,
+                    [self.arr.size(0) - self.idxs[-1]],
+                    dtype=self.idxs.dtype,
+                    device=self.idxs.device,
                 ),
             ]
         )
@@ -96,18 +101,13 @@ class IndexedArray:
         example:
         f( [[a,b,c], [d,e]] ) -> IndexedArray(
                 arr = [a,b,c,d,e],
-                indices = [0,3]
+                idxs = [0,3]
         )
         """
-        # arr: Tensor
-        # for item in data:
-        #     arr.
-
-
 
         arr: ATensor = torch.cat(data)
-        indices: ATensor = torch.cumsum(torch.tensor([0, *map(len, data)]), dim=0)[:-1]
-        return cls(arr=arr, indices=indices)
+        idxs: ATensor = torch.cumsum(torch.tensor([0, *map(len, data)]), dim=0)[:-1]
+        return cls(arr=arr, idxs=idxs)
 
 
 class GPTDataset(Dataset):

maze_transformer/training/mazedataset.py
@@ -192,16 +192,16 @@ class MazeDataset(GPTDataset):
                     f"MazeDataset invalid: {len(self.mazes_objs) = }, {len(self.mazes_tokens) = }"
                 )
 
-        if self.mazes_objs is not None and self.mazes_array.indices is not None:
-            if len(self.mazes_objs) != len(self.mazes_array.indices):
+        if self.mazes_objs is not None and self.mazes_array.idxs is not None:
+            if len(self.mazes_objs) != len(self.mazes_array.idxs):
                 raise ValueError(
-                    f"MazeDataset invalid: {len(self.mazes_objs) = }, {len(self.mazes_array.indices) = }"
+                    f"MazeDataset invalid: {len(self.mazes_objs) = }, {len(self.mazes_array.idxs) = }"
                 )
 
-        if self.mazes_tokens is not None and self.mazes_array.indices is not None:
-            if len(self.mazes_tokens) != len(self.mazes_array.indices):
+        if self.mazes_tokens is not None and self.mazes_array.idxs is not None:
+            if len(self.mazes_tokens) != len(self.mazes_array.idxs):
                 raise ValueError(
-                    f"MazeDataset invalid: {len(self.mazes_tokens) = }, {len(self.mazes_array.indices) = }"
+                    f"MazeDataset invalid: {len(self.mazes_tokens) = }, {len(self.mazes_array.idxs) = }"
                 )
 
     def __getitem__(self, idx: int) -> ATensor[("tokens")]:
@@ -213,15 +213,15 @@ class MazeDataset(GPTDataset):
         # the start of the correct sequence. The min value could be part of the dataset config.
 
         # last element in mazes_array.idxs whose value is smaller than `idx`
-        sequence_idx: int = torch.searchsorted(self.mazes_array.indices, idx) - 1
+        sequence_idx: int = torch.searchsorted(self.mazes_array.idxs, idx) - 1
         # slice the array from the start of the sequence to `idx`, including `idx`
         end_arr_idx: int = min(
             idx + 1,  # up to end of sequence
-            self.mazes_array.indices[sequence_idx]
+            self.mazes_array.idxs[sequence_idx]
             + self.cfg.seq_len_max,  # up to sequence length cutoff
         )
         subseq: ATensor = self.mazes_array.arr[
-                          self.mazes_array.indices[sequence_idx]: end_arr_idx
+            self.mazes_array.idxs[sequence_idx] : end_arr_idx
         ]
         # left-pad the sequence
         return torch.nn.functional.pad(
@@ -338,7 +338,7 @@ class MazeDataset(GPTDataset):
                 f"{path_base}/{self.DISK_SAVE_FILES.tokenized}",
                 **dict(
                     arr=self.mazes_array.arr.cpu().numpy(),
-                    idxs=self.mazes_array.indices.cpu().numpy(),
+                    idxs=self.mazes_array.idxs.cpu().numpy(),
                 ),
             )
 