3f37a2b9fb18ee5c98e647a4965482425390cd98
Rusheb <rusheb.shah@gmail.com>
2023-03-21 12:56:16 +0000

    Weights & Biases integration

--- scripts/plot_loss.py
+++
@@ -1,6 +0,0 @@
-from maze_transformer.evaluation.plot_loss import plot_loss
-
-if __name__ == "__main__":
-    import fire
-
-    fire.Fire(plot_loss)
--- tests/integration/test_train_model.py
+++ tests/integration/test_train_model.py
@@ -1,15 +1,17 @@
 import pytest
 
+from maze_transformer.training.wandb_logger import WandbProject
 from scripts.create_dataset import create_dataset
 from scripts.train_model import train_model
 
 
 @pytest.mark.usefixtures("temp_dir")
-def test_train(temp_dir):
+def test_train_model(temp_dir):
     create_dataset(path_base=str(temp_dir), n_mazes=5, grid_n=3, name="test")
 
     train_model(
         basepath=str(temp_dir / "g3-n5-test"),
+        wandb_project=WandbProject.INTEGRATION_TESTS,
         training_cfg="tiny-v1",
         model_cfg="tiny-v1",
     )
