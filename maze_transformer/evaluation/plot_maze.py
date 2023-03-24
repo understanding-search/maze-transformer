91aea9655896e2106485c6dfd345c103382ecb6a
Can Rager <web.rager@posteo.de>
2023-03-24 02:36:59 +0000

    added fluent interface

--- maze_transformer/evaluation/plot_maze.py
+++ maze_transformer/evaluation/plot_maze.py
@@ -1,166 +1,194 @@
+from __future__ import annotations # for type hinting self as return value
 from dataclasses import dataclass
 
 import matplotlib.pyplot as plt
 import numpy as np
 from muutils.tensor_utils import NDArray
 
-from maze_transformer.generation.latticemaze import LatticeMaze
-
-CoordArray = NDArray["coords", np.int8]
-
-# Set size of unit.
-# Each "unit" consists of a node and the right and lower adjacent wall/connection. Its area is ul * ul.
-# Wall thickness is fixed to 1, so this parameter sets the ratio of node length to wall thickness
-ul = 14
-
-
-# Maze conversion functions (No plt involved)
-
-
-def as_img(maze: LatticeMaze) -> NDArray["x y", bool]:
-    """
-     Build an image to visualise the maze.
-     Each "unit" consists of a node and the right and lower adjacent wall/connection. Its area is ul * ul.
-     - Nodes are displayed as white squares of area: (ul-1) * (ul-1)
-     - Walls are displayed as black recktangles of area: 1 * (ul-1)
-     - Connections are displayed as light grey or white rectangles of area: 1 * (ul-1); color is depending on show_connections argument
-
-     Axes definition:
-    (0,0)     y
-      -|----------->
-     x |
-       |
-       |
-       v
-
-     Returns a matrix of side length (ul) * n + 1 where n is the number of nodes.
-    """
-
-    # Set color of connections (using plt.imshow(cmap='grey))
-    connection_color = white_color * 0.93
-
-    # Set up the background (walls everywhere)
-    img: NDArray["x y", int] = np.zeros(
-        (
-            maze.grid_shape[0] * ul + 1,
-            maze.grid_shape[1] * ul + 1,
-        ),
-        dtype=int,
-    )
-
-    # Draw nodes and connections by iterating through lattice
-    for x in range(maze.grid_shape[0]):
-        for y in range(maze.grid_shape[1]):
-            # Draw node
-            img[x * ul + 1 : (x + 1) * ul, y * ul + 1 : (y + 1) * ul] = white_color
-
-            # Down connection
-            if maze.connection_list[0, x, y]:
-                img[(x + 1) * ul, y * ul + 1 : (y + 1) * ul] = connection_color
-
-            # Right connection
-            if maze.connection_list[1, x, y]:
-                img[x * ul + 1 : (x + 1) * ul, (y + 1) * ul] = connection_color
-
-    return img
-
-
-def points_transform_to_img(points: CoordArray) -> CoordArray:
-    """transform points to img coordinates"""
-
-    #####
-    # Swapping x, y axes here may be deprecated with resolution of issue #73
-    points = np.array([(x, y) for (y, x) in points])
-    #####
-
-    return ul * (points + 0.5)
-
-
-# Image rendering functions (plt involved)
-
-
-def plot_maze(maze: LatticeMaze, show: bool = True) -> None:
-    """ul : ratio of wall thickness and node length"""
-
-    img = as_img(maze)
-    plt.imshow(img, cmap="gray")
-
-    # Plot labels
-    tick_arr = np.arange(maze.grid_shape[0])
-    plt.xticks(ul * (tick_arr + 0.5), tick_arr)
-    plt.yticks(ul * (tick_arr + 0.5), tick_arr)
-    plt.xlabel("y")
-    plt.ylabel("x")
-    if show:
-        plt.show()
-
-
-def plot_path(maze: LatticeMaze, path: NDArray, show: bool = True) -> None:
-    # print(m)
-    # show the maze
-    path_transformed = points_transform_to_img(path)
-
-    # dimension swap -  we are translating from array notation to cartesian coordinates
-    plt.plot(path_transformed[:, 1], path_transformed[:, 0], "-", color="red")
-    # mark endpoints
-    plt.plot([path_transformed[0][1]], [path_transformed[0][0]], "o", color="red")
-    plt.plot([path_transformed[-1][1]], [path_transformed[-1][0]], "x", color="red")
-    # show actual maze
-    plot_maze(maze, show=False)
-
-    if show:
-        plt.show()
-
+from maze_transformer.generation.latticemaze import LatticeMaze, CoordArray
 
 @dataclass
 class PathFormat:
     """formatting options for path plot
     (path_true, "true", "-", "red")
     """
-
-    path: NDArray
+    path: CoordArray
     label: str | None = None
     fmt: str | None = None
     color: str | None = None
+    line_width: float | None = None
     quiver_kwargs: dict | None = (None,)
 
+class MazePlot():
+    """Class for displaying mazes and paths
+    
+    UNIT_LENGTH: Set ratio between node size and wall thickness in image.
+    Wall thickness is fixed to 1px
+    A "unit" consists of a single node and the right and lower connection/wall.
+    Example: ul = 14 yields 13:1 ratio between node size and wall thickness
+    """
+    UNIT_LENGTH = 14
+    DEFAULT_PREDICTED_PATH_COLORS = ["tab:blue", "tab:green", "tab:purple", "tab:orange", "tab:olive", "tab:cyan"]
 
-def plot_multi_paths(
-    maze: LatticeMaze,
-    paths: list[PathFormat | tuple | list],
-    show: bool = True,
-) -> None:
-    # plot paths
-    for pf in paths:
-        if isinstance(pf, (tuple, list)):
-            pf = PathFormat(*pf)
 
-        p_transformed = points_transform_to_img(np.array(pf.path))
-        if pf.quiver_kwargs is not None:
-            # Pyplot uses Cartesian coordinates (x horixontal and y vertical)
-            # But our mazes and paths use array notation (row, col)
-            # So we swap the dimensions here
-            x: NDArray = p_transformed[:, 1]
-            y: NDArray = p_transformed[:, 0]
-            plt.quiver(
-                x[:-1],
-                y[:-1],
-                x[1:] - x[:-1],
-                y[1:] - y[:-1],
-                scale_units="xy",
-                angles="xy",
-                scale=1,
-                color=pf.color,
-                **pf.quiver_kwargs,
+    def __init__(self, maze: LatticeMaze) -> None:
+        self.maze: LatticeMaze = maze
+        self.fig = plt.figure()
+        self.ax = self.fig.add_subplot(1,1,1)
+        self.true_path: PathFormat = None
+        self.predicted_paths: list = []
+
+    def add_true_path(self,
+                      path: CoordArray,
+                      label: str = "true path",
+                      fmt: str = "--",
+                      color: str = "red",
+                      line_width: float = 2.5,
+                      quiver_kwargs: dict | None = None,
+
+        ) -> MazePlot:
+        """
+        Recieve true path and formatting preferences from input and save as dict in true_path variable.
+        """
+        self.true_path = PathFormat(
+            path = path,
+            label = label,
+            fmt = fmt,
+            color = color,
+            line_width = line_width,
+            quiver_kwargs = quiver_kwargs
+        )
+        return self
+    
+    def add_predicted_path(self,
+                           path: CoordArray,
+                           label: str | None = None,
+                           fmt: str = ':',
+                           color: str | None = None,
+                           line_width: float = 2,
+                           quiver_kwargs: dict | None = {'width': 0.015}
+        ) -> MazePlot:
+        """
+        Recieve predicted path and formatting preferences from input and save in predicted_path list.
+        Default formatting depends on nuber of paths already saved in predicted path list.
+        """
+        if label is None:
+            label = "predicted path {path_num}".format( path_num = len(self.predicted_paths) + 1)
+        if color is None:
+            color = self.DEFAULT_PREDICTED_PATH_COLORS[len(self.predicted_paths)]
+        
+        self.predicted_paths.append(
+            PathFormat(
+                path = path,
+                label = label,
+                fmt = fmt,
+                color = color,
+                line_width = line_width,
+                quiver_kwargs = quiver_kwargs
             )
+        )
+        return self
+
+    def _latticemaze_to_img(self) -> NDArray["row col", bool]:
+        """
+        Build an image to visualise the maze.
+        Each "unit" consists of a node and the right and lower adjacent wall/connection. Its area is ul * ul.
+        - Nodes are displayed as white squares of area: (ul-1) * (ul-1)
+        - Walls are displayed as black recktangles of area: 1 * (ul-1)
+        - Connections are displayed as light grey or white rectangles of area: 1 * (ul-1); color is depending on show_connections argument
+
+        Axes definition:
+        (0,0)     y, col
+        ----|----------->
+        x,  | 
+        row |
+            |
+            v
+
+        Returns a matrix of side length (ul) * n + 1 where n is the number of nodes.
+        """
+        # Set color of connections (using plt.imshow(cmap='grey))
+        node_color = 1
+        connection_color = node_color * 0.93
+
+        # Set up the background (walls everywhere)
+        img: NDArray["row col", int] = np.zeros(
+            (
+                self.maze.grid_shape[0] * self.UNIT_LENGTH + 1,
+                self.maze.grid_shape[1] * self.UNIT_LENGTH + 1,
+            ),
+            dtype=float,
+        )
+
+        # Draw nodes and connections by iterating through lattice
+        for row in range(self.maze.grid_shape[0]):
+            for col in range(self.maze.grid_shape[1]):
+                # Draw node
+                img[row * self.UNIT_LENGTH + 1 : (row + 1) * self.UNIT_LENGTH, col * self.UNIT_LENGTH + 1 : (col + 1) * self.UNIT_LENGTH] = node_color
+
+                # Down connection
+                if self.maze.connection_list[0, row, col]:
+                    img[(row + 1) * self.UNIT_LENGTH, col * self.UNIT_LENGTH + 1 : (col + 1) * self.UNIT_LENGTH] = connection_color
+
+                # Right connection
+                if self.maze.connection_list[1, row, col]:
+                    img[row * self.UNIT_LENGTH + 1 : (row + 1) * self.UNIT_LENGTH, (col + 1) * self.UNIT_LENGTH] = connection_color
+
+        return img
+
+    def _rowcol_to_coord(self, points: CoordArray) -> NDArray:
+        """transform points to img coordinates"""
+        points = np.array([(x, y) for (y, x) in points])
+        return self.UNIT_LENGTH * (points + 0.5)
+
+
+    # Image rendering functions (plt involved)
+
+    def _plot_maze(self) -> None:
+        img = self._latticemaze_to_img()
+        self.ax.imshow(img, cmap="gray", vmin=0, vmax=1)
+
+    def _plot_path(self, pf: PathFormat) -> None:
+        p_transformed = self._rowcol_to_coord(pf.path)
+        if pf.quiver_kwargs is not None:
+                x: NDArray = p_transformed[:, 0]
+                y: NDArray = p_transformed[:, 1]
+                self.ax.quiver(
+                    x[:-1],
+                    y[:-1],
+                    x[1:] - x[:-1],
+                    y[1:] - y[:-1],
+                    scale_units="xy",
+                    angles="xy",
+                    scale=1,
+                    color=pf.color,
+                    **pf.quiver_kwargs,
+                )
         else:
-            plt.plot(*zip(*p_transformed), pf.fmt, color=pf.color, label=pf.label)
+            self.ax.plot(*zip(*p_transformed),
+                         pf.fmt,
+                         lw=pf.line_width,
+                         color=pf.color,
+                         label=pf.label)
         # mark endpoints
-        plt.plot([p_transformed[0][1]], [p_transformed[0][0]], "o", color=pf.color)
-        plt.plot([p_transformed[-1][1]], [p_transformed[-1][0]], "x", color=pf.color)
+        self.ax.plot([p_transformed[0][1]], [p_transformed[0][0]], "o", color=pf.color)
+        self.ax.plot([p_transformed[-1][1]], [p_transformed[-1][0]], "x", color=pf.color)
 
-    # show actual maze
-    plot_maze(maze, show=False)
+    def show(self) -> None:
+        """Plot the maze and paths."""
 
-    if show:
+        self._plot_maze()
+
+        if self.true_path is not None:
+            self._plot_path(self.true_path)
+        for path in self.predicted_paths:
+            self._plot_path(path)
+            
+        # Plot labels
+        tick_arr = np.arange(self.maze.grid_shape[0])
+        self.ax.set_xticks(self.UNIT_LENGTH * (tick_arr + 0.5), tick_arr)
+        self.ax.set_yticks(self.UNIT_LENGTH * (tick_arr + 0.5), tick_arr)
+        # self.ax.set_xlabel("y")
+        # self.ax.set_ylabel("x")
+
         plt.show()
