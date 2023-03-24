dfc40973b0bd40ae82d95dba1540626964773bbd
Can Rager <web.rager@posteo.de>
2023-03-24 02:39:43 +0000

    make format

--- maze_transformer/evaluation/plot_maze.py
+++ maze_transformer/evaluation/plot_maze.py
@@ -1,17 +1,20 @@
-from __future__ import annotations # for type hinting self as return value
+from __future__ import annotations  # for type hinting self as return value
+
 from dataclasses import dataclass
 
 import matplotlib.pyplot as plt
 import numpy as np
 from muutils.tensor_utils import NDArray
 
-from maze_transformer.generation.latticemaze import LatticeMaze, CoordArray
+from maze_transformer.generation.latticemaze import CoordArray, LatticeMaze
+
 
 @dataclass
 class PathFormat:
     """formatting options for path plot
     (path_true, "true", "-", "red")
     """
+
     path: CoordArray
     label: str | None = None
     fmt: str | None = None
@@ -19,72 +22,83 @@ class PathFormat:
     line_width: float | None = None
     quiver_kwargs: dict | None = (None,)
 
-class MazePlot():
+
+class MazePlot:
     """Class for displaying mazes and paths
-    
+
     UNIT_LENGTH: Set ratio between node size and wall thickness in image.
     Wall thickness is fixed to 1px
     A "unit" consists of a single node and the right and lower connection/wall.
     Example: ul = 14 yields 13:1 ratio between node size and wall thickness
     """
-    UNIT_LENGTH = 14
-    DEFAULT_PREDICTED_PATH_COLORS = ["tab:blue", "tab:green", "tab:purple", "tab:orange", "tab:olive", "tab:cyan"]
 
+    UNIT_LENGTH = 14
+    DEFAULT_PREDICTED_PATH_COLORS = [
+        "tab:blue",
+        "tab:green",
+        "tab:purple",
+        "tab:orange",
+        "tab:olive",
+        "tab:cyan",
+    ]
 
     def __init__(self, maze: LatticeMaze) -> None:
         self.maze: LatticeMaze = maze
         self.fig = plt.figure()
-        self.ax = self.fig.add_subplot(1,1,1)
+        self.ax = self.fig.add_subplot(1, 1, 1)
         self.true_path: PathFormat = None
         self.predicted_paths: list = []
 
-    def add_true_path(self,
-                      path: CoordArray,
-                      label: str = "true path",
-                      fmt: str = "--",
-                      color: str = "red",
-                      line_width: float = 2.5,
-                      quiver_kwargs: dict | None = None,
-
-        ) -> MazePlot:
+    def add_true_path(
+        self,
+        path: CoordArray,
+        label: str = "true path",
+        fmt: str = "--",
+        color: str = "red",
+        line_width: float = 2.5,
+        quiver_kwargs: dict | None = None,
+    ) -> MazePlot:
         """
         Recieve true path and formatting preferences from input and save as dict in true_path variable.
         """
         self.true_path = PathFormat(
-            path = path,
-            label = label,
-            fmt = fmt,
-            color = color,
-            line_width = line_width,
-            quiver_kwargs = quiver_kwargs
+            path=path,
+            label=label,
+            fmt=fmt,
+            color=color,
+            line_width=line_width,
+            quiver_kwargs=quiver_kwargs,
         )
         return self
-    
-    def add_predicted_path(self,
-                           path: CoordArray,
-                           label: str | None = None,
-                           fmt: str = ':',
-                           color: str | None = None,
-                           line_width: float = 2,
-                           quiver_kwargs: dict | None = {'width': 0.015}
-        ) -> MazePlot:
+
+    def add_predicted_path(
+        self,
+        path: CoordArray,
+        label: str | None = None,
+        fmt: str = ":",
+        color: str | None = None,
+        line_width: float = 2,
+        quiver_kwargs: dict | None = {"width": 0.015},
+    ) -> MazePlot:
         """
         Recieve predicted path and formatting preferences from input and save in predicted_path list.
         Default formatting depends on nuber of paths already saved in predicted path list.
         """
         if label is None:
-            label = "predicted path {path_num}".format( path_num = len(self.predicted_paths) + 1)
+            label = "predicted path {path_num}".format(
+                path_num=len(self.predicted_paths) + 1
+            )
         if color is None:
             color = self.DEFAULT_PREDICTED_PATH_COLORS[len(self.predicted_paths)]
-        
+
         self.predicted_paths.append(
             PathFormat(
-                path = path,
-                label = label,
-                fmt = fmt,
-                color = color,
-                line_width = line_width,
-                quiver_kwargs = quiver_kwargs
+                path=path,
+                label=label,
+                fmt=fmt,
+                color=color,
+                line_width=line_width,
+                quiver_kwargs=quiver_kwargs,
             )
         )
         return self
@@ -100,7 +114,7 @@ class MazePlot():
         Axes definition:
         (0,0)     y, col
         ----|----------->
-        x,  | 
+        x,  |
         row |
             |
             v
@@ -124,15 +138,24 @@ class MazePlot():
         for row in range(self.maze.grid_shape[0]):
             for col in range(self.maze.grid_shape[1]):
                 # Draw node
-                img[row * self.UNIT_LENGTH + 1 : (row + 1) * self.UNIT_LENGTH, col * self.UNIT_LENGTH + 1 : (col + 1) * self.UNIT_LENGTH] = node_color
+                img[
+                    row * self.UNIT_LENGTH + 1 : (row + 1) * self.UNIT_LENGTH,
+                    col * self.UNIT_LENGTH + 1 : (col + 1) * self.UNIT_LENGTH,
+                ] = node_color
 
                 # Down connection
                 if self.maze.connection_list[0, row, col]:
-                    img[(row + 1) * self.UNIT_LENGTH, col * self.UNIT_LENGTH + 1 : (col + 1) * self.UNIT_LENGTH] = connection_color
+                    img[
+                        (row + 1) * self.UNIT_LENGTH,
+                        col * self.UNIT_LENGTH + 1 : (col + 1) * self.UNIT_LENGTH,
+                    ] = connection_color
 
                 # Right connection
                 if self.maze.connection_list[1, row, col]:
-                    img[row * self.UNIT_LENGTH + 1 : (row + 1) * self.UNIT_LENGTH, (col + 1) * self.UNIT_LENGTH] = connection_color
+                    img[
+                        row * self.UNIT_LENGTH + 1 : (row + 1) * self.UNIT_LENGTH,
+                        (col + 1) * self.UNIT_LENGTH,
+                    ] = connection_color
 
         return img
 
@@ -140,7 +163,6 @@ class MazePlot():
         """transform points to img coordinates"""
         points = np.array([(x, y) for (y, x) in points])
         return self.UNIT_LENGTH * (points + 0.5)
-
 
     # Image rendering functions (plt involved)
 
@@ -151,28 +173,32 @@ class MazePlot():
     def _plot_path(self, pf: PathFormat) -> None:
         p_transformed = self._rowcol_to_coord(pf.path)
         if pf.quiver_kwargs is not None:
-                x: NDArray = p_transformed[:, 0]
-                y: NDArray = p_transformed[:, 1]
-                self.ax.quiver(
-                    x[:-1],
-                    y[:-1],
-                    x[1:] - x[:-1],
-                    y[1:] - y[:-1],
-                    scale_units="xy",
-                    angles="xy",
-                    scale=1,
-                    color=pf.color,
-                    **pf.quiver_kwargs,
-                )
+            x: NDArray = p_transformed[:, 0]
+            y: NDArray = p_transformed[:, 1]
+            self.ax.quiver(
+                x[:-1],
+                y[:-1],
+                x[1:] - x[:-1],
+                y[1:] - y[:-1],
+                scale_units="xy",
+                angles="xy",
+                scale=1,
+                color=pf.color,
+                **pf.quiver_kwargs,
+            )
         else:
-            self.ax.plot(*zip(*p_transformed),
-                         pf.fmt,
-                         lw=pf.line_width,
-                         color=pf.color,
-                         label=pf.label)
+            self.ax.plot(
+                *zip(*p_transformed),
+                pf.fmt,
+                lw=pf.line_width,
+                color=pf.color,
+                label=pf.label,
+            )
         # mark endpoints
         self.ax.plot([p_transformed[0][1]], [p_transformed[0][0]], "o", color=pf.color)
-        self.ax.plot([p_transformed[-1][1]], [p_transformed[-1][0]], "x", color=pf.color)
+        self.ax.plot(
+            [p_transformed[-1][1]], [p_transformed[-1][0]], "x", color=pf.color
+        )
 
     def show(self) -> None:
         """Plot the maze and paths."""
@@ -183,7 +209,7 @@ class MazePlot():
             self._plot_path(self.true_path)
         for path in self.predicted_paths:
             self._plot_path(path)
-            
+
         # Plot labels
         tick_arr = np.arange(self.maze.grid_shape[0])
         self.ax.set_xticks(self.UNIT_LENGTH * (tick_arr + 0.5), tick_arr)
