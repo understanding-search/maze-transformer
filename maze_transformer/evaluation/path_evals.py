bf407c599a330c87c577e133d84ddb3fb748a9bf
mivanit <mivanits@umich.edu>
2023-04-01 09:41:46 -0600

    added type checking and general cleanup of `PathEvals.all_functions()`
    see dicussion: https://github.com/AISC-understanding-search/maze-transformer/pull/134#discussion_r1149359824

--- maze_transformer/evaluation/path_evals.py
+++ maze_transformer/evaluation/path_evals.py
@@ -1,17 +1,18 @@
-from typing import Callable, Iterable, TypeAlias
+import types
+import typing
 
 import numpy as np
-from muutils.tensor_utils import NDArray
+from jaxtyping import Int
 
 from maze_transformer.generation.constants import Coord, CoordTup
 from maze_transformer.generation.latticemaze import LatticeMaze
 
 # pylint: disable=unused-argument
-MazePath: TypeAlias = NDArray["node x_y_pos", int]
-PathEvalFunction = Callable[[LatticeMaze, MazePath, MazePath], float]
+MazePath: typing.TypeAlias = Int[np.ndarray, "node x_y_pos"]
+PathEvalFunction = typing.Callable[[LatticeMaze, MazePath, MazePath], float]
 
 
-def path_as_segments_iter(path: MazePath) -> Iterable[tuple]:
+def path_as_segments_iter(path: MazePath) -> typing.Iterable[tuple]:
     """
     Iterate over the segments of a path.
     """
@@ -24,11 +25,52 @@ def path_as_segments_iter(path: MazePath) -> Iterable[tuple]:
 
 
 class PathEvals:
-    """array path based eval functions. first path is always the "ground truth" path"""
+    """array path based eval functions. first path is always the "ground truth" path
+
+    if you add a util function that isnt a path eval function, add it to the EXCLUDED_MEMBERS list
+    """
+
+    _EXCLUDED_MEMBERS: typing.Sequence[str] = tuple(
+        [
+            "all_functions",
+            # the following two are excluded automatically because they start with "_"
+            # this comment should be left in to why they are absent from this list
+            # "_EXCLUDED_MEMBERS",
+            # "_assert_is_eval_function",
+        ]
+    )
+
+    @staticmethod
+    def _assert_is_eval_function(func: typing.Callable) -> None:
+        """asserts that the function is a valid **path eval function**"""
+        assert isinstance(func, typing.Callable), "func must be callable"
+        assert isinstance(func, types.FunctionType), "func must be a static function"
+
+        hints: dict[str, type] = typing.get_type_hints(func)
+        assert hints["return"] is float, "path evals must return a float"
+        assert hints["maze"] is LatticeMaze, "maze must be a LatticeMaze"
+        assert hints["solution"] is MazePath, "solution must be a MazePath"
+        assert hints["prediction"] is MazePath, "prediction must be a MazePath"
+
+    @classmethod
+    def all_functions(cls) -> dict[str, PathEvalFunction]:
+        """returns a dict of all the path eval functions
+
+        will throw except if any non_excluded member is not a valid path eval function
+        """
+        output: dict[str, PathEvalFunction] = dict()
+        for name, func in cls.__dict__.items():
+            if not name.startswith("_") and name not in cls._EXCLUDED_MEMBERS:
+                cls._assert_is_eval_function(func)
+                output[name] = func
+
+        return output
 
     @staticmethod
     def node_overlap(
-        maze: LatticeMaze, solution: MazePath, prediction: MazePath, /
+        maze: LatticeMaze,
+        solution: MazePath,
+        prediction: MazePath,
     ) -> float:
         """number of shared nodes (any order) / total number of (unique) nodes"""
         if len(prediction) <= 1:
@@ -45,7 +87,9 @@ class PathEvals:
 
     @staticmethod
     def num_connections_adjacent_lattice(
-        maze: LatticeMaze, solution: MazePath, prediction: MazePath, /
+        maze: LatticeMaze,
+        solution: MazePath,
+        prediction: MazePath,
     ) -> float:
         """number of the connections in prediction which actually connect nodes that are adjacent on the lattice, ignoring if they are adjacent on the maze"""
         if len(prediction) <= 1:
@@ -60,7 +104,9 @@ class PathEvals:
 
     @staticmethod
     def fraction_connections_adjacent_lattice(
-        maze: LatticeMaze, solution: MazePath, prediction: MazePath, /
+        maze: LatticeMaze,
+        solution: MazePath,
+        prediction: MazePath,
     ) -> float:
         """fraction of the connections in prediction which actually connect nodes that are adjacent on the lattice, ignoring if they are adjacent on the maze"""
 
@@ -70,7 +116,9 @@ class PathEvals:
 
     @staticmethod
     def num_connections_adjacent(
-        maze: LatticeMaze, solution: MazePath, prediction: MazePath, /
+        maze: LatticeMaze,
+        solution: MazePath,
+        prediction: MazePath,
     ) -> float:
         """number of connections in prediction which are are valid paths on the maze"""
 
@@ -86,22 +134,12 @@ class PathEvals:
 
     @staticmethod
     def fraction_connections_adjacent(
-        maze: LatticeMaze, solution: MazePath, prediction: MazePath, /
+        maze: LatticeMaze,
+        solution: MazePath,
+        prediction: MazePath,
     ) -> float:
         """fraction of connections in prediction which are are valid paths on the maze"""
 
         return PathEvals.num_connections_adjacent(maze, solution, prediction) / len(
             prediction
         )
-
-    @classmethod
-    def all_functions(cls) -> dict[str, PathEvalFunction]:
-        excluded = ["all_functions"]
-
-        return {
-            **{
-                name: func
-                for name, func in cls.__dict__.items()
-                if not name.startswith("_") and name not in excluded
-            }
-        }
