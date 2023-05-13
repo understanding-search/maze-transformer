e3ba65e5596bd9a118be968c0db0e2ea3f1980b6
mivanit <mivanits@umich.edu>
2023-05-13 02:50:29 -0600

    more wip collected datasets

--- maze_transformer/dataset/collected_dataset.py
+++ maze_transformer/dataset/collected_dataset.py
@@ -34,13 +34,21 @@ from maze_transformer.utils.stable_hash import stable_hash
 @serializable_dataclass(
     kw_only=True, properties_to_serialize=_MAZEDATASET_PROPERTIES_TO_SERIALIZE
 )
-class MazeDatasetCollectionConfig(MazeDatasetConfig):
+class MazeDatasetCollectionConfig(GPTDatasetConfig):
     """maze dataset collection configuration, including tokenizers and shuffle"""
     
     maze_dataset_configs: list[MazeDatasetConfig] = serializable_field(
-        serialization_fn=lambda configs: [config.to_dict() for config in configs],
-        loading_fn=lambda data: [MazeDatasetConfig.from_dict(config) for config in data["maze_dataset_configs"]]
+        serialization_fn=lambda configs: [config.serialize() for config in configs],
+        loading_fn=lambda data: [MazeDatasetConfig.load(config) for config in data["maze_dataset_configs"]]
     )
+
+    @property
+    def n_mazes(self) -> int:
+        return sum(config.n_mazes for config in self.maze_dataset_configs)
+    
+    @property
+    def max_grid_n(self) -> int:
+        return max(config.max_grid_n for config in self.maze_dataset_configs)
     
 
 class MazeDatasetCollection(GPTDataset):
@@ -62,6 +70,10 @@ class MazeDatasetCollection(GPTDataset):
     @cached_property
     def dataset_cum_lengths(self) -> list[int]:
         return list(itertools.accumulate(self.dataset_lengths))
+    
+    @cached_property
+    def mazes(self) -> list[LatticeMaze]:
+        return list(itertools.chain.from_iterable(dataset.mazes for dataset in self.maze_datasets))
 
     def __len__(self) -> int:
         return sum(len(dataset) for dataset in self.maze_datasets)
@@ -72,4 +84,37 @@ class MazeDatasetCollection(GPTDataset):
         return self.maze_datasets[dataset_idx][
             index - self.dataset_cum_lengths[dataset_idx - 1]
         ]
-        
+    
+    @classmethod
+    def generate(cls, cfg: MazeDatasetCollectionConfig, **kwargs) -> 'MazeDatasetCollection':
+        datasets = [
+            MazeDataset.generate(config, **kwargs) 
+            for config in cfg.maze_dataset_configs
+        ]
+        return cls(cfg, datasets)
+
+    @classmethod
+    def download(cls, cfg: MazeDatasetCollectionConfig, **kwargs) -> 'MazeDatasetCollection':
+        datasets = [
+            MazeDataset.download(config, **kwargs) 
+            for config in cfg.maze_dataset_configs
+        ]
+        return cls(cfg, datasets)
+
+    def serialize(self) -> JSONitem:
+        return dict(
+            cfg = self.cfg.serialize(),
+            maze_datasets = [dataset.serialize() for dataset in self.maze_datasets],
+        )
+
+    @classmethod
+    def load(cls, data: JSONitem) -> 'MazeDatasetCollection':
+        cfg = MazeDatasetCollectionConfig.from_dict(data["cfg"])
+        datasets = [MazeDataset.load(dataset_data) for dataset_data in data["maze_datasets"]]
+        return cls(cfg, datasets)
+
+    def update_self_config(self) -> None:
+        self.cfg.n_mazes = len(self)
+        for dataset in self.maze_datasets:
+            dataset.update_self_config()
+
