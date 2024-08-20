8311eb8ae3c35b200c00935185ed5edfb60770ac
mivanit <mivanits@umich.edu>
2024-08-20 15:54:34 -0600

    move training tests to test_train_model.py

--- tests/integration/test_eval_model.py
+++ tests/integration/test_eval_model.py
@@ -12,55 +12,12 @@ from pathlib import Path
 import numpy as np
 import pytest
 from maze_dataset import CoordTup, MazeDataset
-from zanj import ZANJ
-from zanj.torchutil import assert_model_cfg_equality
 
 from maze_transformer.evaluation.eval_model import evaluate_model, predict_maze_paths
 from maze_transformer.evaluation.path_evals import PathEvals
-from maze_transformer.test_helpers.assertions import assert_model_output_equality
 from maze_transformer.training.config import ConfigHolder, ZanjHookedTransformer
-from maze_transformer.training.train_model import TrainingResult, train_model
-from maze_transformer.training.train_save_files import TRAIN_SAVE_FILES
-from maze_transformer.training.wandb_logger import WandbProject
 
 temp_dir: Path = Path("tests/_temp/test_eval_model")
-
-
-def test_model_loading():
-    zanj: ZANJ = ZANJ(
-        custom_settings={
-            "_load_state_dict_wrapper": {"recover_exact": True, "fold_ln": False}
-        }
-    )
-    # get config
-    cfg: ConfigHolder = ConfigHolder.get_config_multisource(
-        cfg_names=("test-g3-n5-a_dfs-h73257", "nano-v1", "test-v1"),
-    )
-    # train model
-    result: TrainingResult = train_model(
-        base_path=temp_dir,
-        wandb_project=WandbProject.INTEGRATION_TESTS,
-        cfg=cfg,
-        do_generate_dataset=True,
-    )
-    model_ret: ZanjHookedTransformer = result.model
-
-    # load model
-    model_load_auto: ZanjHookedTransformer = zanj.read(
-        result.output_path / TRAIN_SAVE_FILES.model_final_zanj
-    )
-
-    # Load model manually without folding
-    assert cfg == model_ret.zanj_model_config
-    assert_model_cfg_equality(model_ret, model_load_auto)
-
-    vocab_size: int = len(model_ret.zanj_model_config.tokenizer)
-    assert_model_output_equality(
-        model_ret,
-        model_load_auto,
-        check_argsort_equality=(vocab_size > 2048),
-    )
-    # assert_model_exact_equality(model_ret, model_load_auto)
 
 
 def test_predict_maze_paths():
--- tests/integration/test_train_model.py
+++ tests/integration/test_train_model.py
@@ -1,6 +1,15 @@
+from pathlib import Path
+
+from zanj import ZANJ
+from zanj.torchutil import assert_model_cfg_equality
+
+from maze_transformer.test_helpers.assertions import assert_model_output_equality
 from maze_transformer.training.config import ConfigHolder, ZanjHookedTransformer
 from maze_transformer.training.train_model import TrainingResult, train_model
+from maze_transformer.training.train_save_files import TRAIN_SAVE_FILES
 from maze_transformer.training.wandb_logger import WandbProject
+
+temp_dir: Path = Path("tests/_temp/test_train_model")
 
 
 def test_train_model():
@@ -9,7 +18,7 @@ def test_train_model():
     )
     cfg.dataset_cfg.n_mazes = 10
     result: TrainingResult = train_model(
-        base_path="tests/_temp/test_train_model",
+        base_path=temp_dir,
         wandb_project=WandbProject.INTEGRATION_TESTS,
         cfg=cfg,
         do_generate_dataset=True,
@@ -17,3 +26,40 @@ def test_train_model():
 
     assert isinstance(result.model, ZanjHookedTransformer)
     assert result.model.zanj_model_config == cfg
+
+
+def test_model_loading():
+    zanj: ZANJ = ZANJ(
+        custom_settings={
+            "_load_state_dict_wrapper": {"recover_exact": True, "fold_ln": False}
+        }
+    )
+    # get config
+    cfg: ConfigHolder = ConfigHolder.get_config_multisource(
+        cfg_names=("test-g3-n5-a_dfs-h73257", "nano-v1", "test-v1"),
+    )
+    # train model
+    result: TrainingResult = train_model(
+        base_path=temp_dir,
+        wandb_project=WandbProject.INTEGRATION_TESTS,
+        cfg=cfg,
+        do_generate_dataset=True,
+    )
+    model_ret: ZanjHookedTransformer = result.model
+
+    # load model
+    model_load_auto: ZanjHookedTransformer = zanj.read(
+        result.output_path / TRAIN_SAVE_FILES.model_final_zanj
+    )
+
+    # Load model manually without folding
+    assert cfg == model_ret.zanj_model_config
+    assert_model_cfg_equality(model_ret, model_load_auto)
+
+    vocab_size: int = len(model_ret.zanj_model_config.tokenizer)
+    assert_model_output_equality(
+        model_ret,
+        model_load_auto,
+        check_argsort_equality=(vocab_size > 2048),
+    )
+    # assert_model_exact_equality(model_ret, model_load_auto)
